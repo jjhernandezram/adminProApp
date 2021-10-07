003c7185b85fd949e40296451b8e42364fb115ec
jjhernandezram <jjhernandezram@gmail.com>
2021-10-07 00:48:19 -0500

    Implemented graphics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 import { AuthModule } from './auth/auth.module';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './404/notfound.component';
 
 @NgModule({
   declarations: [AppComponent, NotfoundComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AuthModule,
-    PagesModule,
-    NgxChartsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, PagesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
