68473d4563bc73138b02d5c3088483da8653a6b8
jjhernandezram <jjhernandezram@gmail.com>
2021-10-07 00:36:53 -0500

    Implemented graphics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 import { AuthModule } from './auth/auth.module';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './404/notfound.component';
 
 @NgModule({
   declarations: [AppComponent, NotfoundComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, PagesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    PagesModule,
+    NgxChartsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/graphics/graphics.component.ts
+++ src/app/pages/graphics/graphics.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { sum } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-graphics',
